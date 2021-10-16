e166f3fee995ab73b3d39c4ab0e336f247903a54
Kevin Phan <kphan489@gmail.com>
2021-10-15 20:29:07 -0500

    Minor refactor on Homescreen

--- ShoppingApp/ViewControllers/HomeViewController.swift
+++ ShoppingApp/ViewControllers/HomeViewController.swift
@@ -18,8 +18,58 @@ class HomeViewController: UIViewController {
         ScrollViewImages(outfitImage: UIImage(imageLiteralResourceName: "4.Outfit"))
     ]
     
+    //Button states
     var topButtonIsPressed: Bool = false
     var bottomButtonIsPressed: Bool = false
+    
+    //Homescreen background setup
+    lazy var backgroundModel: UIImageView = {
+        let modelBackgroundView = UIImageView()
+        modelBackgroundView.contentMode = UIView.ContentMode.scaleAspectFit
+        modelBackgroundView.image = UIImage(imageLiteralResourceName: "model")
+    
+        return modelBackgroundView
+    }()
+    
+    func initBackGroundModel() {
+        view.addSubview(backgroundModel)
+
+        backgroundModel.translatesAutoresizingMaskIntoConstraints = false
+        backgroundModel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        backgroundModel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        backgroundModel.widthAnchor.constraint(equalToConstant: view.frame.width).isActive = true
+        backgroundModel.heightAnchor.constraint(equalToConstant: view.frame.height).isActive = true
+    }
+    
+    //Scrollview setup
+    lazy var scrollView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        
+        layout.itemSize = CGSize(width: view.safeAreaLayoutGuide.layoutFrame.width,
+                                 height: view.safeAreaLayoutGuide.layoutFrame.height)
+        layout.scrollDirection = .horizontal
+        layout.minimumLineSpacing = 0
+        layout.minimumInteritemSpacing = 0
+        
+        let collectionView1 = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        
+        collectionView1.register(HomepageScrollViewCustomCell.self, forCellWithReuseIdentifier: HomepageScrollViewCustomCell.identifier)
+        
+        return collectionView1
+    }()
+    
+    func initScrollView() {
+        view.addSubview(scrollView)
+        scrollView.backgroundColor = UIColor.clear
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.isPagingEnabled = true
+        scrollView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        scrollView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+        scrollView.dataSource = self
+    }
+    
     
     //Top shirt button setup
     lazy var topButton: HomescreenClothesSelectorButton = {
@@ -41,49 +91,42 @@ class HomeViewController: UIViewController {
         return button
     }()
     
+    func buttonConstraints(){
+        let stack = UIStackView(arrangedSubviews: [topButton, bottomButton])
+        stack.axis = .vertical
+        stack.spacing = 0
+        stack.distribution = .fillEqually
+        
+        view.addSubview(stack)
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -20).isActive = true
+        stack.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -15).isActive = true
+        stack.widthAnchor.constraint(equalToConstant: 25).isActive = true
+        stack.heightAnchor.constraint(equalToConstant: 80).isActive = true
+    }
+    
+    func connectScrollViewWithButton () {
+        if bottomButtonIsPressed == false {
+            scrollView.isScrollEnabled = false
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = UIConfiguration.homeScreenTitle
-        
+        self.view = view
+
         //Change background color when implementing button to see position.
         view.backgroundColor = .white
-        let homescreenModel = UIImage(named: "model")
+        //let homescreenModel = UIImage(named: "model")
         
         //Homescreen background setup
-        let modelBackgroundView:UIImageView = UIImageView()
-        modelBackgroundView.contentMode = UIView.ContentMode.scaleAspectFit
-        modelBackgroundView.image = homescreenModel
-        self.view = view
-        view.addSubview(modelBackgroundView)
-        modelBackgroundView.translatesAutoresizingMaskIntoConstraints = false
-        modelBackgroundView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        modelBackgroundView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        modelBackgroundView.widthAnchor.constraint(equalToConstant: view.frame.width).isActive = true
-        modelBackgroundView.heightAnchor.constraint(equalToConstant: view.frame.height).isActive = true
-        
+        initBackGroundModel()
+
         //Scroll view setup
-        let layout = UICollectionViewFlowLayout()
-        layout.itemSize = CGSize(width: view.safeAreaLayoutGuide.layoutFrame.width,
-                                 height: view.safeAreaLayoutGuide.layoutFrame.height)
-        layout.scrollDirection = .horizontal
-        layout.minimumLineSpacing = 0
-        layout.minimumInteritemSpacing = 0
+        initScrollView()
         
-        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        
-        collectionView.register(HomepageScrollViewCustomCell.self, forCellWithReuseIdentifier: HomepageScrollViewCustomCell.identifier)
-        
-        view.addSubview(collectionView)
-        
-        collectionView.backgroundColor = UIColor.clear
-        collectionView.translatesAutoresizingMaskIntoConstraints = false
-        collectionView.isPagingEnabled = true
-        collectionView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
-        collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        collectionView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
-        
-        collectionView.dataSource = self
+        connectScrollViewWithButton()
         
         //Double tap implementation
         view.isUserInteractionEnabled = true
@@ -93,7 +136,8 @@ class HomeViewController: UIViewController {
         view.addGestureRecognizer(tapGesture)
         
         //Button setup
-        setUpContraints()
+        buttonConstraints()
+        
         topButton.addTarget(self, action: #selector(topButtonClicked), for: .touchUpInside)
         bottomButton.addTarget(self, action: #selector(bottomButtonClicked), for: .touchUpInside)
     }
@@ -106,21 +150,7 @@ class HomeViewController: UIViewController {
     
     //MARK: Helpers
     
-    func setUpContraints(){
-        let stack = UIStackView(arrangedSubviews: [topButton, bottomButton])
-        stack.axis = .vertical
-        stack.spacing = 0
-        stack.distribution = .fillEqually
-        
-        view.addSubview(stack)
-        stack.translatesAutoresizingMaskIntoConstraints = false
-        
-        stack.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor, constant: -20).isActive = true
-        stack.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -15).isActive = true
-        stack.widthAnchor.constraint(equalToConstant: 25).isActive = true
-        stack.heightAnchor.constraint(equalToConstant: 80).isActive = true
-    }
-    
+
     //Change button state
     @objc func topButtonClicked(){
         topButtonIsPressed = true
@@ -128,12 +158,14 @@ class HomeViewController: UIViewController {
         topButton.setImage(UIImage(named: "top-filled-icon"), for: .normal)
         bottomButton.setImage(UIImage(named: "bottoms-icon"), for: .normal)
         
+        //scrollView1.isScrollEnabled = false
+        
         print("Top button pressed")
     }
     
     @objc func bottomButtonClicked(){
-        bottomButtonIsPressed = true
         topButtonIsPressed = false
+        bottomButtonIsPressed = true
         topButton.setImage(UIImage(named: "top-icon"), for: .normal)
         bottomButton.setImage(UIImage(named: "bottoms-filled-icon"), for: .normal)
         
